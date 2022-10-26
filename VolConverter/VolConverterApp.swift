93055c777840d8cc631104d2bbb8f416dd95148b
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-10-26 10:48:38 +0700

    Initial Commit

---
+++ VolConverter/VolConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolConverterApp.swift
+//  VolConverter
+//
+//  Created by Benny Rinaldo on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct VolConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
