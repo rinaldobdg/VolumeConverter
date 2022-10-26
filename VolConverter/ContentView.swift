b0c42ba0366d6974c674e15070f3586805845d67
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-10-26 13:34:32 +0700

    first commit

--- VolConverter/ContentView.swift
+++ VolConverter/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var mesInput = UnitVolume.milliliters
+    @State private var mesOutput = UnitVolume.liters
+    @State private var value: Double = 0.0
+    
+    let measures = [UnitVolume.milliliters, UnitVolume.liters, UnitVolume.cups, UnitVolume.pints, UnitVolume.gallons]
+    
+    private var result: Double {
+        let inputResult = Measurement(value: value, unit: mesInput)
+        let outputResult = inputResult.converted(to: mesOutput)
+        return outputResult.value
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Insert a measurement value", value: $value, format: .number)
+                        .keyboardType(.numberPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Picker("Choose your input unit", selection: $mesInput) {
+                        ForEach(measures, id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Enter your input unit:")
+                }
+                
+                Section {
+                    Picker("Choose your output unit", selection: $mesOutput) {
+                        ForEach(measures, id:\.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    Text(result.formatted())
+                } header: {
+                    Text("Select your output unit")
+                }
+            }
+            .navigationTitle("Volume Converter")
         }
-        .padding()
     }
 }
 
